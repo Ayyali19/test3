76782901669b555e731b94281955b2a16a5f6b95
Ayyali19 <ayyalikalli@gmail.com>
2025-03-18 15:50:35 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -8,11 +8,12 @@ public class Test3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
-
-                     float = 20f;
-		
-		     float m= 10.3f;
-		     
-		
+		Stiring name="mike";
+		int x=100;
+		int y=200;
+		double z=10.3;
+		boolean isPresent=false;
+		float m=10.3f;
+		SpringApplication.run(Test5Application.class,args);
 	}
 }
