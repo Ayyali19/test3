5d0e3043ab85c3a2edf04074f137e17384e8585f
Ayyali19 <ayyalikalli@gmail.com>
2025-03-18 16:02:03 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,18 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
-		SpringApplicas
-		SpringApplication.run(Test3Application.class, args);
-		Stiring name="mike";
-		int x=100;
-		int y=200;
-		char x='a';
-    Stiring name="mike";
+                Stiring name="mike";
 		int x=100;
 		int y=200;
 		double z=10.3;
 		boolean isPresent=false;
-		float m=10.3f;
+		char x= 'a';
 		SpringApplication.run(Test5Application.class,args);
 	}
 }
