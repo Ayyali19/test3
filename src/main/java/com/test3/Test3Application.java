61623d88fdd62e74038c7220a28467cf793156c2
Ayyali19 <ayyalikalli@gmail.com>
2025-03-18 11:34:35 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -8,12 +8,7 @@ public class Test3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
-
-                 double s = 1000;
-                 int s= 200;
-
-                 float x=90.0f;
-                int x=90;
+		int x=100;
 
 		
 	}
